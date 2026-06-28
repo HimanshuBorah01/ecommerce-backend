97d62fb895b23b58245fbce3e891e0caaa14f61e
Himanshu Borah <himanshuborah9954@gmail.com>
2026-06-28 21:43:25 +0530

    feat: add wishlist management APIs

--- src/controllers/wishlist.controller.js
+++ src/controllers/wishlist.controller.js
@@ -34,7 +34,7 @@ export async function addWishlist(req, res) {
     if (isAlreadyExists) {
       return res.status(400).json({
         success: false,
-        message: "Product already exist in this wishlist",
+        message: "Product already exists in the wishlist",
       });
     }
 
@@ -44,7 +44,81 @@ export async function addWishlist(req, res) {
 
     return res.status(200).json({
       success: true,
-      message: "Product added to wishlist successfully ",
+      message: "Product added to wishlist successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
+
+// get wishlist
+export async function getWishlist(req, res) {
+  try {
+    const wishlist = await wishlistModel
+      .findOne({
+        user: req.user._id,
+      })
+      .populate({
+        path: "products",
+      });
+
+    if (!wishlist) {
+      return res.status(200).json({
+        success: true,
+        wishlist: [],
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      wishlist,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
+
+export async function removeWishlist(req, res) {
+  try {
+    const { productId } = req.params;
+
+    const wishlist = await wishlistModel.findOne({
+      user: req.user._id,
+    });
+
+    if (!wishlist) {
+      return res.status(404).json({
+        success: false,
+        message: "Wishlist not found",
+      });
+    }
+
+    const isProductExist = wishlist.products.some(
+      (id) => id.toString() === productId,
+    );
+
+    if (!isProductExist) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found in wishlist",
+      });
+    }
+
+    wishlist.products = wishlist.products.filter(
+      (id) => id.toString() !== productId,
+    );
+
+    await wishlist.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Product remove from wishlist successfully",
     });
   } catch (error) {
     return res.status(500).json({
--- src/routes/wishlist.routes.js
+++ src/routes/wishlist.routes.js
@@ -1,6 +1,10 @@
 import express from "express";
-import { addWishlist } from "../controllers/wishlist.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
+import {
+  addWishlist,
+  getWishlist,
+  removeWishlist,
+} from "../controllers/wishlist.controller.js";
 
 const router = express.Router();
 
@@ -11,6 +15,19 @@ router.post(
   authMiddleware.protect,
   authMiddleware.userOnly,
   addWishlist,
+);
+
+// get user wishlist
+// GET /api/v1/wishlist
+router.get("/", authMiddleware.protect, authMiddleware.userOnly, getWishlist);
+
+// remove product from wishlist
+// DELETE /api/v1/wishlist/:productId
+router.delete(
+  "/:productId",
+  authMiddleware.protect,
+  authMiddleware.userOnly,
+  removeWishlist,
 );
 
 export default router;
